aedd3387233d04384d298fb46f4a96522c87bddd
Stefani Fiykova <sfiykova@gmail.com>
2024-10-16 10:57:26 +0300

    Add change to App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import { ThemeProvider } from "./context/ThemeContext";
 import Portfolio from "../src/pages/Portfolio";
 import BackToTopButton from "./components/BackToTopButton";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import "./styles/main.scss";
 
 function App() {
   return (
-    <div className="App">
-      <ThemeProvider>
-        <Portfolio />
-        <BackToTopButton />
-      </ThemeProvider>
-    </div>
+    <Router basename="/portfolio">
+      <div className="App">
+        <ThemeProvider>
+          <Portfolio />
+          <BackToTopButton />
+        </ThemeProvider>
+      </div>
+    </Router>
   );
 }
 
